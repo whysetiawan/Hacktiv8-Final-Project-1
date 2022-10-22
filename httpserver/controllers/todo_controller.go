479bc88bd59955ab01223cb7d35b494b51a35aa0
whysetiawan <32702491+whysetiawan@users.noreply.github.com>
2022-10-22 16:10:44 +0700

    Docs/swagger (#6)
    * feat:integrate create todo
    
    * chore:added configuration for env
    
    * docs:added swagger and generic types HttpSuccess
    
    Co-authored-by: wahyu.setiawan <wahyu.setiawan@sicepat.com>

--- httpserver/controllers/todo_controller.go
+++ httpserver/controllers/todo_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"final-project-1/httpserver/dto"
 	"final-project-1/httpserver/services"
+	"final-project-1/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,22 +24,38 @@ func NewTodoController(todoService services.TodoService) *todoController {
 	}
 }
 
+// CreateTodo godoc
+// @Tags    Todo
+// @Summary create a todo
+// @Param   todo body     dto.CreateTodoDto true "Create Todo DTO"
+// @Success 200  {object} utils.HttpSuccess[models.TodoModel]
+// @Failure 400  {object} utils.HttpError
+// @Failure 500  {object} utils.HttpError
+// @Router  /todo [post]
 func (c *todoController) CreateTodo(ctx *gin.Context) {
 	var dto dto.CreateTodoDto
 	err := ctx.ShouldBindJSON(&dto)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, "Bad Request")
+		ctx.JSON(http.StatusBadRequest, utils.NewHttpError("Bad Request", err.Error()))
 		return
 	}
 
 	todo, err := c.todoService.CreateTodo(dto)
 
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, "Error")
+		ctx.JSON(http.StatusInternalServerError, utils.NewHttpError("Internal Server Error", err.Error()))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, todo)
+	ctx.JSON(http.StatusOK, utils.NewHttpSuccess("Todo Created", todo))
 }
 
+// DeleteTodo godoc
+// @Tags    Todo
+// @Summary create a todo
+// @Param   id  path     int true "Todo ID"
+// @Success 200 {object} utils.HttpSuccess[models.TodoModel]
+// @Failure 400 {object} utils.HttpError
+// @Failure 500 {object} utils.HttpError
+// @Router  /todo [delete]
 func (c *todoController) DeleteTodo(ctx *gin.Context) {}
--- main.go
+++ main.go
@@ -8,10 +8,28 @@ import (
 	"final-project-1/httpserver/services"
 	"log"
 
+	_ "final-project-1/docs"
+
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"github.com/joho/godotenv" // swagger embed files
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
+// @title          Swagger Example API
+// @version        1.0
+// @description    This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name  API Support
+// @contact.url   http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url  http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host     localhost:3030
+// @BasePath /api
 func main() {
 
 	err := godotenv.Load(".env.dev")
@@ -21,13 +39,16 @@ func main() {
 	}
 
 	app := gin.Default()
+	appRoute := app.Group("/api")
 	db, _ := config.Connect()
 
 	todoRepository := repositories.NewTodoRepository(db)
 	todoService := services.NewTodoService(todoRepository)
 	todoController := controllers.NewTodoController(todoService)
 
-	routers.TodoRouter(app, todoController)
+	routers.TodoRouter(appRoute, todoController)
+
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	app.Run(":3030")
 }
